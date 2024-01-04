681863c519eebdedac62f08f0480dc54c85c63f9
SHARMA <sharmajhonny@gmail.com>
2024-01-04 21:15:52 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,24 @@
+// console.log("Learning javascipt");/
+
+require('dotenv').config()
+
+const express = require('express')
+const app = express()
+const port = 3000
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+app.get('/prince',(req,res)=>{
+    res.send('Learning Backend in progress')
+})
+
+app.get('/learning',function(req,res){
+    res.send('second page loading')
+})
+
+
+app.listen(process.env.port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
